3e533a1c8ab6ac97358b3acdb2a7e5616833477b
llting592 <llting592@gmail.com>
2022-02-19 22:04:01 -0500

    third commit, deleted route files, got app working

--- server.js
+++ server.js
@@ -8,13 +8,74 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
+let notes = require("./db/db.json");
 
-//Use route files
-require('./routes')(app);
-require('./htmlroute')(app);
+//route
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Display notes
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(notes);
+  });
+});
+
 
 // Setup app listener
 app.listen(PORT, function() {
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 });  
+
+//create new note
+app.post("/api/notes", function (req, res) {
+  let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  let id = randLetter + Date.now();
+  let newNote = {
+    id: id,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  console.log(typeof notes);
+  notes.push(newNote);
+  const stringifyNote = JSON.stringify(notes);
+  res.json(notes);
+  fs.writeFile("db/db.json", stringifyNote, (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("Note successfully saved to db.json");
+    }
+  });
+});
+
+// Delete note
+app.delete("/api/notes/:id", function (req, res) {
+  let noteID = req.params.id;
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    let updatedNotes = JSON.parse(data).filter((note) => {
+      console.log("note.id", note.id);
+      console.log("noteID", noteID);
+      return note.id !== noteID;
+    });
+    notes = updatedNotes;
+    const stringifyNote = JSON.stringify(updatedNotes);
+    fs.writeFile("db/db.json", stringifyNote, (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Note successfully deleted from db.json");
+      }
+    });
+    res.json(stringifyNote);
+  });
+});
+
+// Catch all error route
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
